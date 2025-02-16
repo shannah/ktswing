5dfd75238b498267252ea58a4e8940ccf61f58e8
Steve Hannah <steve@weblite.ca>
2025-02-16 13:14:42 -0800

    added support for imagePanel in swingx

--- swingx/src/main/kotlin/ca/weblite/ktswing/swingx/SwingxBuilders.kt
+++ swingx/src/main/kotlin/ca/weblite/ktswing/swingx/SwingxBuilders.kt
@@ -2,13 +2,35 @@ package ca.weblite.ktswing.swingx
 
 import ca.weblite.ktswing.extensions.getFactoryForComponent
 import ca.weblite.ktswing.extensions.isAutoAddEnabled
+import org.jdesktop.swingx.JXImagePanel
 import org.jdesktop.swingx.JXSearchField
 import java.awt.Component
 import java.awt.Container
+import java.net.URL
+import javax.swing.ImageIcon
 
 fun Container.searchField(init: JXSearchField.() -> Unit = {}): JXSearchField {
     val factory = this.getFactoryForComponent(JXSearchField::class.java)
     val component = if (factory == null) JXSearchField() else factory()
+    component.init()
+    if (this.isAutoAddEnabled()) {
+        this.add(component as Component)
+    }
+    return component
+}
+
+fun Container.imagePanel(init: JXImagePanel.() -> Unit = {}): JXImagePanel {
+    val factory = this.getFactoryForComponent(JXImagePanel::class.java)
+    val component = if (factory == null) JXImagePanel() else factory()
+    component.init()
+    if (this.isAutoAddEnabled()) {
+        this.add(component as Component)
+    }
+    return component
+}
+
+fun Container.imagePanel(imageUrl: URL, init: JXImagePanel.() -> Unit = {}): JXImagePanel {
+    val component = JXImagePanel(imageUrl)
     component.init()
     if (this.isAutoAddEnabled()) {
         this.add(component as Component)
